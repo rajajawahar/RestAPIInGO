36893203e654f1f66870a6948be4711fc154fce9
Raja Jawahar <rajajawahar77@gmail.com>
2018-05-03 08:16:06 +0530

    Http Get Method added for Listing all the employees

--- employee.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type Employee struct {
-	ID        int64  `db:"id" json:"id"`
-	Firstname string `db:"firstname" json:"firstname"`
-	Lastname  string `db:"lastname" json:"lastname"`
-	Salary    string `db:"salary" json:"salary"`
-}
--- main.go
+++ main.go
@@ -6,18 +6,41 @@ import (
 
 	"gopkg.in/gorp.v1"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	r := gin.Default()
 
+	r.Use(Cors())
+
+	v1 := r.Group("api/v1")
+	{
+		v1.GET("/employees", GetEmployees)
+	}
+
+	r.Run(":8080")
 }
 
 var dbmap = initDatabase()
 
 func initDatabase() *gorp.DbMap {
 	db, error := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/")
-	checkErr(error, "sql.Open failed")
+	if error != nil {
+		panic(error)
+	}
+	defer db.Close()
+
+	_, error = db.Exec("CREATE DATABASE " + "SalesApp")
+	if error != nil {
+		panic(error)
+	}
+	_, error = db.Exec("USE " + "SalesApp")
+	if error != nil {
+		panic(error)
+	}
+
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
 	dbmap.AddTableWithName(Employee{}, "Employee").SetKeys(true, "Id")
 	error = dbmap.CreateTablesIfNotExists()
@@ -29,4 +52,31 @@ func checkErr(err error, msg string) {
 	if err != nil {
 		log.Fatalln(msg, err)
 	}
+}
+
+type Employee struct {
+	Id        int64  `db:"id" json:"id"`
+	Firstname string `db:"firstname" json:"firstname"`
+	Lastname  string `db:"lastname" json:"lastname"`
+	Salary    string `db:"salary" json:"salary"`
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+
+func GetEmployees(c *gin.Context) {
+	var employees []Employee
+	_, err := dbmap.Select(&employees, "SELECT * FROM employee")
+
+	if err == nil {
+		c.JSON(200, employees)
+	} else {
+		c.JSON(404, gin.H{"error": "no employee(s) into the table"})
+	}
+
+	// curl -i http://localhost:8080/api/v1/users
 }
